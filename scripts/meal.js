91775ca487e3e5e6f4f61af2cf11a165d6d269b1
zavi3 <duna.blue@gmail.com>
2020-09-13 13:41:08 +0900

    workout and meal are update

--- scripts/meal.js
+++ scripts/meal.js
@@ -1,12 +1,13 @@
 'use strict';
-const userTallInput = document.getElementById('cm');
+const userTallInput2 = document.getElementById('cm2');
+const userMoveInput = document.getElementById('Move')
 const KcalButton = document.getElementById('Kcal');
 const KcalDivided = document.getElementById('Kcal-area')
 const header = document.createElement('h3');
 const paragraph = document.createElement('p');
 const paragraph2 = document.createElement('div');
-const userTall = userTallInput.value;
-const userMove = document.forms["meal"].Move.value;
+const userTall2 = userTallInput2.value;
+const userMove = userMoveInput.value;
 
 
 /**
@@ -20,7 +21,7 @@ function removeAllChildren(element) {
 }
 
 KcalButton.onclick = () => {
-  if (userTall.length === 0 ) { //身長または活動量が空の時は
+  if (userTall2.length === 0 ) { //身長または活動量が空の時は
     
     header.innerText = '計算方法';
     KcalDivided.appendChild(header);
@@ -28,7 +29,7 @@ KcalButton.onclick = () => {
     paragraph.innerText = '標準体重（ 身長(m) × 身長(m) × 22 ）× 活動量'
     KcalDivided.appendChild(paragraph);
     
-  } else if (userTall.indexOf('１') !== -1)　{ //半角英数でない入力の時は
+  } else if (userTall2.indexOf('１') !== -1)　{ //半角英数でない入力の時は
     
     header.innerText = '全角で入力されました';
     KcalDivided.appendChild(header);
@@ -37,12 +38,12 @@ KcalButton.onclick = () => {
     KcalDivided.appendChild(paragraph);
     
   } else {
-    removeAllChildren(resultDivided); //消す
+    removeAllChildren(KcalDivided); //消す
 
     header.innerText = '１日の摂取カロリー';
     KcalDivided.appendChild(header);
 
-    const dayKcal = kcalBMI(userTall); //１日の摂取カロリー
+    const dayKcal = kcalBMI(userTall2); //１日の摂取カロリー
     paragraph.innerText = dayKcal;
     KcalDivided.appendChild(paragraph);
   }
@@ -50,26 +51,28 @@ KcalButton.onclick = () => {
 
 /**
  * 身長と活動量の値を渡すと計算結果を返す関数
- * @param {Int} userTall ユーザーの身長
+ * @param {Int} userTall2 ユーザーの身長
  * @param {Int} userMove ユーザーの活動量
  * @return {Int} 計算結果
  */
 
-function kcalBMI(userTall) {
-  const userBmi = userTall / 100 * userTall / 100;
-  const bmi22kcal = userBmi * 22 * userMove;
+function kcalBMI(userTall2) {
+  const userTall2 = userTallInput2.value;
+  const userMove = userMoveInput.value;
+  const userBmi2 = userTall2 / 100 * userTall2 / 100;
+  const bmi22kcal = userBmi2 * 22 * userMove;
   const kcalBMI22 = Math.round(bmi22kcal);
 
   let dayKcal = 'あなたの１日摂取カロリーの適量は、{kcalBMI22}kcalです。';
 
   dayKcal = dayKcal.replace(/\{kcalBMI22\}/g, kcalBMI22);
 
-  console.log(dayKcal);
+  console.log(kcalBMI22);
   console.log(userMove);
   return dayKcal;
 }
 
-userTallInput.onkeydown = (event) => {
+userTallInput2.onkeydown = (event) => {
   if (event.key === 'Enter') {
     KcalButton.onclick();
   // TODO ボタンのonclick() 処理を呼び出す
--- scripts/workout.js
+++ scripts/workout.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const userWeightInput = document.getElementById('kg');
+const userWeightInput2 = document.getElementById('kg2');
+const userMETsInput = document.getElementById("Run");
 const METsButton = document.getElementById('METs');
 const METsDivided = document.getElementById('METs-area')
 const header = document.createElement('h3');
 const paragraph = document.createElement('p');
 const paragraph2 = document.createElement('div');
-const userWeight = userWeightInput.value;
-const userMETs = document.forms["Running"].Run.value;
+const userWeight2 = userWeightInput2.value;
+const userMETs = userMETsInput.value;
 
 /**
  * 指定したHTML要素の子要素を全て削除する
@@ -20,8 +21,8 @@ function removeAllChildren(element) {
 }
 
 METsButton.onclick = () => {
-  const userWeight = userWeightInput.value;
-  if (userWeight.length === 0) { //体重が空の時は
+  const userWeight2 = userWeightInput2.value;
+  if (userWeight2.length === 0) { //体重が空の時は
     
       header.innerText = '運動30分あたりの消費カロリー 計算方法';
       METsDivided.appendChild(header);
@@ -29,7 +30,7 @@ METsButton.onclick = () => {
       paragraph.innerText = '体重(kg) × 1.05 × (運動METs - 1METs) ÷ 2';
       METsDivided.appendChild(paragraph);
       
-  } else if ( userWeight.indexOf('１') !== -1)　{ //半角英数でない入力の時は
+  } else if ( userWeight2.indexOf('１') !== -1)　{ //半角英数でない入力の時は
       
       header.innerText = '全角で入力されました';
       METsDivided.appendChild(header);
@@ -38,12 +39,12 @@ METsButton.onclick = () => {
       METsDivided.appendChild(paragraph);
       
   } else {
-      removeAllChildren(resultDivided); //消す
+      removeAllChildren(METsDivided); //消す
 
       header.innerText = '運動30分あたりの消費カロリー';
       METsDivided.appendChild(header);
 
-      const runKcal = kcalMETs(userWeight); //運動消費カロリー
+      const runKcal = kcalMETs(userWeight2); //運動消費カロリー
       paragraph.innerText = runKcal;
       METsDivided.appendChild(paragraph);
   }
@@ -51,14 +52,14 @@ METsButton.onclick = () => {
 
 /**
  * 身長と活動量の値を渡すと計算結果を返す関数
- * @param {Int} userWeight ユーザーの体重
+ * @param {Int} userWeight2 ユーザーの体重
  * @param {Int} userMETs ユーザーの運動メッツ数
  * @return {Int} 計算結果
  */
 
-function kcalMETs(userWeight) {
-  const userMETs = document.forms["Running"].Run.value;
-  const user30run = userWeight * 1.05 * userMETs / 2;
+function kcalMETs(userWeight2) {
+  const userMETs = userMETsInput.value;
+  const user30run = userWeight2 * 1.05 * userMETs / 2;
   const run30Kcal = Math.round(user30run);
 
   let runKcal = 'あなたの30分の運動消費カロリーは、{run30Kcal}kcalです。';
@@ -66,11 +67,12 @@ function kcalMETs(userWeight) {
   runKcal = runKcal.replace(/\{run30Kcal\}/g, run30Kcal);
 
   console.log(runKcal);
+  console.log(userWeight2);
   console.log(userMETs);
   return runKcal;
 }
 
-userWeightInput.onkeydown = (event) => {
+userWeightInput2.onkeydown = (event) => {
   if (event.key === 'Enter') {
     METsButton.onclick();
   // TODO ボタンのonclick() 処理を呼び出す
